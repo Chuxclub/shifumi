e7d0dd0cf5cbcd535a5b31ea155f227e232ab57d
Florian <florian-1992@hotmail.fr>
2018-09-27 21:56:03 +0200

    Prototype code has been cleaned up and runs pretty well, more features will be added after further analysis

--- player_vs_ai_module.cpp
+++ player_vs_ai_module.cpp
@@ -7,6 +7,66 @@
 #include "termcolor.hpp"
 #include "player_vs_ai_module.h"
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CORE GAME ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void player_vs_ai_mode()
+{
+    char new_game = 'y', choice;
+    int rounds = 0, player_won_rounds = 0, computer_won_rounds = 0;
+
+    //Assuming the player wants to play at least once, loop can be broken
+    //after having won or lost a battle and chosen to exit
+    while (new_game == 'y')
+    {
+        char computer_choice = ai_choice();
+
+        std::cout << "\n\n";
+        std::cout << "Your choice, human: ";
+        std::cin >> choice;
+        user_input_test(choice);
+
+        player_won_rounds += user_win_count(choice, computer_choice);
+        computer_won_rounds += computer_win_count(choice, computer_choice);
+        print_winner(choice, computer_choice);
+
+        rounds++;
+
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ End game ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+        if(rounds == 3)
+        {
+            //If draw, enters deathmatch
+            deathmatch(player_won_rounds, computer_won_rounds, choice, computer_choice);
+
+            //Else, determines winner of the battle
+            print_battle_winner(player_won_rounds, computer_won_rounds);
+
+            std::cout << "\n\n";
+            std::cout << "Do you want a new game? Press 'y' to continue, any key to exit: ";
+            std::cin >> new_game;
+
+            //All counters are reset after a battle is over
+            rounds = 0;
+            player_won_rounds = 0;
+            computer_won_rounds = 0;
+        }
+
+        /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GAME PARTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+//Selects a random 'char' in a vector in order to simulate a random choice for shifumi
+//Pre-conditions : no arguments, just a subroutine
+//Post-conditions : returns a 'char'
 char ai_choice()
 {
     vector<char> possible_outcomes = {'s', 'p', 'c'};
@@ -20,6 +80,63 @@ char ai_choice()
     return computer_choice;
 }
 
+//Checks if user input is valid, valid inputs are: 'p', 's' or 'c', enters a loop
+//if input is not valid
+void user_input_test(char &choice)
+{
+    while (choice != 'p' && choice != 's' && choice != 'c')
+    {
+        std::cout << termcolor::red << "\n" << "Wrong input!" << termcolor::reset;
+        cin.clear();
+        cin.ignore(1000, '\n');
+        std::cout << "\n" << "Input a valid character ('p', 's' or 'c') : ";
+        std::cin >> choice;
+        std::cout << "\n";
+    }
+}
+
+//Checks if player won the round, if yes, adds +1 to the number of won rounds
+//If no, adds nothing
+int user_win_count(char choice, char computer_choice)
+{
+    int a = 0;
+
+    if (choice == 's' && computer_choice == 'c')
+        a++;
+
+    else if (choice == 'c' && computer_choice == 'p')
+        a++;
+
+    else if (choice == 'p' && computer_choice == 's')
+        a++;
+
+    else
+        a;
+
+    return a;
+}
+
+//Same function as above but with the ai
+int computer_win_count(char choice, char computer_choice)
+{
+    int a = 0;
+
+    if (choice == 'c' && computer_choice == 's')
+        a++;
+
+    else if (choice == 'p' && computer_choice == 'c')
+        a++;
+
+    else if (choice == 's' && computer_choice == 'p')
+        a++;
+
+    else
+        a;
+
+    return a;
+}
+
+//Compares user input with ai random choice and outputs an appropriate message
 void print_winner (char choice, char computer_choice)
 {
     if (choice == computer_choice)
@@ -47,104 +164,35 @@ void print_winner (char choice, char computer_choice)
         std::cout << termcolor::red <<"Wrong input!" << termcolor::reset;
 }
 
-int user_win_count(char choice, char computer_choice)
+//If there's a draw after the third round, enters a loop until someone wins
+//Hence to exit the loop, won rounds between the player and ai must be different
+void deathmatch (int &player_won_rounds, int &computer_won_rounds, char &choice, char &computer_choice)
 {
-    int a = 0;
-
-    if (choice == 's' && computer_choice == 'c')
-        a++;
-
-    else if (choice == 'c' && computer_choice == 'p')
-        a++;
-
-    else if (choice == 'p' && computer_choice == 's')
-        a++;
-
-    else
-        a;
-
-    return a;
-}
-
-int computer_win_count(char choice, char computer_choice)
-{
-    int a = 0;
-
-    if (choice == 'c' && computer_choice == 's')
-        a++;
-
-    else if (choice == 'p' && computer_choice == 'c')
-        a++;
-
-    else if (choice == 's' && computer_choice == 'p')
-        a++;
-
-    else
-        a;
-
-    return a;
-}
-
-void user_input_test(char &choice)
-{
-    while (choice != 'p' && choice != 's' && choice != 'c')
+    while (player_won_rounds == computer_won_rounds)
     {
-        std::cout << termcolor::red <<"Wrong input!" << termcolor::reset;
-        cin.clear();
-        cin.ignore(1000, '\n');
-        std::cout << "Input a valid character ('p', 's' or 'c') : ";
-        std::cin >> choice;
-    }
-}
-
-void player_vs_ai_mode()
-{
-    char new_game = 'y', choice;
-    int rounds = 0, player_won_rounds = 0, computer_won_rounds = 0;
-
-    while (new_game == 'y')
-    {
-        char computer_choice = ai_choice();
+        computer_choice = ai_choice();
 
         std::cout << "\n\n";
-        std::cout << "Your choice, human: ";
+        std::cout << "Last chance, human: ";
         std::cin >> choice;
+
         user_input_test(choice);
 
-        print_winner(choice, computer_choice);
         player_won_rounds += user_win_count(choice, computer_choice);
         computer_won_rounds += computer_win_count(choice, computer_choice);
-        rounds++;
-
-        if(rounds == 3)
-        {
-            while (player_won_rounds == computer_won_rounds)
-            {
-                std::cout << "\n\n";
-                std::cout << "Last chance, human: ";
-                std::cin >> choice;
-
-                user_input_test(choice);
-
-                print_winner(choice, computer_choice);
-                player_won_rounds += user_win_count(choice, computer_choice);
-                computer_won_rounds += computer_win_count(choice, computer_choice);
-            }
-
-            if (player_won_rounds < computer_won_rounds)
-                std::cout << "\n\n" << termcolor::red << "I won the game human..." << termcolor::reset;
-
-            else
-                std::cout << "\n\n" << termcolor::green << "How is it even possible!" << termcolor::reset;
-
-            std::cout << "\n\n";
-            std::cout << "Do you want a new game? Press 'y' to continue, any key to exit: ";
-            std::cin >> new_game;
-
-            rounds = 0;
-            player_won_rounds = 0;
-            computer_won_rounds = 0;
-        }
+        print_winner(choice, computer_choice);
 
     }
 }
+
+//Prints an appropriate message depending on who won more battles
+void print_battle_winner(int player_won_rounds, int computer_won_rounds)
+{
+    if (player_won_rounds < computer_won_rounds)
+        std::cout << "\n\n" << termcolor::red << "I won the game human..." << termcolor::reset;
+    else
+        std::cout << "\n\n" << termcolor::green << "How is it even possible!" << termcolor::reset;
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
--- player_vs_ai_module.h
+++ player_vs_ai_module.h
@@ -5,11 +5,31 @@
 #ifndef SHIFUMI_PLAYER_VS_AI_MODULE_H
 #define SHIFUMI_PLAYER_VS_AI_MODULE_H
 
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CORE GAME ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+void player_vs_ai_mode();
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+
+
+
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ GAME PARTS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
 char ai_choice();
-void print_winner (char choice, char computer_choice);
+
+void user_input_test(char &choice);
+
+
 int user_win_count(char choice, char computer_choice);
 int computer_win_count(char choice, char computer_choice);
-void user_input_test(char &choice);
-void player_vs_ai_mode();
+void print_winner (char choice, char computer_choice);
+
+
+void deathmatch (int &player_won_rounds, int &computer_won_rounds, char &choice, char &computer_choice);
+void print_battle_winner(int player_won_rounds, int computer_won_rounds);
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
 #endif //SHIFUMI_PLAYER_VS_AI_MODULE_H
